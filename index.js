966d49e3d1e0f2caafccc7377c9be3518c17fd85
Benjamin Ikeh <b.ikeh@yahoo.com>
2021-06-18 16:06:52 +0000

    sessions

--- index.js
+++ index.js
@@ -3,8 +3,47 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const bcrypt = require('bcrypt-nodejs');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
+
+const MongoStore = require('connect-mongo');
 
 const app = express();
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
+
+const dbstring = 'mongodb://localhost:27017/tutorial_db';
+
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+const connection = mongoose.createConnection(dbstring, dbOptions);
+if (connection){
+    console.log("connected")
+}
+
+const sessionStore = new MongoStore({
+    mongoUrl: dbstring,
+    collection: 'sessions'
+});
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 24 // Equals 1 days
+    }
+}));
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello World (Sessions)<h1>')
+});
+
+const PORT = 6000
+app.listen(PORT, ()=> {
+    console.log(`app is running on ${PORT}`);
+})
