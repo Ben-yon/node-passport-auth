dc756e7cf29099bf0cd406f7ee5fd97c59321c78
Benjamin Ikeh <b.ikeh@yahoo.com>
2021-07-06 15:44:30 +0000

    routes

--- index.js
+++ index.js
@@ -14,32 +14,32 @@ const app = express();
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 
-const dbstring = 'mongodb://localhost:27017/tutorial_db';
+// const dbstring = 'mongodb://localhost:27017/tutorial_db';
 
-const dbOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
+// const dbOptions = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }
 
-const connection = mongoose.createConnection(dbstring, dbOptions);
-if (connection){
-    console.log("connected")
-}
+// const connection = mongoose.createConnection(dbstring, dbOptions);
+// if (connection){
+//     console.log("connected")
+// }
 
-const sessionStore = new MongoStore({
-    mongoUrl: dbstring,
-    collection: 'sessions'
-});
+// const sessionStore = new MongoStore({
+//     mongoUrl: dbstring,
+//     collection: 'sessions'
+// });
 
-app.use(session({
-    secret: 'some secret',
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore,
-    cookie: {
-        maxAge: 1000 * 60 * 24 // Equals 1 days
-    }
-}));
+// app.use(session({
+//     secret: 'some secret',
+//     resave: false,
+//     saveUninitialized: true,
+//     store: sessionStore,
+//     cookie: {
+//         maxAge: 1000 * 60 * 24 // Equals 1 days
+//     }
+// }));
 
 // app.get('/', (req, res, next) => {
 //     if (req.session.viewCount){
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-const router = require('express').Router;
-
+const router = require('express').Router();
 const passport = require('passport');
 const passwordUtils = require('../lib/passportUtils');
 const connection = require('../config/database');
-
+const User = connection.models.User;
 
 
 /**
@@ -12,17 +11,15 @@ const connection = require('../config/database');
 
 // TODO 
 
-router.post('/login', passport.authenticate('local'), (req, res,next) = {
+// router.post('/login', passport.authenticate('local'), (req, res,next) = {});
 
-})
-
-router.post('/register', (req, res, next) =>{
-    const saltHash = passwordUtils.genPassword(req.body.pw);
+router.post('/register', (req, res, next) => {
+    const saltHash = passwordUtils.genPassword(req.body.pword);
 
     const salt = saltHash.salt;
     const hash = saltHash.hash;
 
-    const newUser = new user({
+    const newUser = new User({
         username: req.body.uname,
         hash: hash,
         salt: salt
@@ -39,14 +36,19 @@ router.post('/register', (req, res, next) =>{
 /**
  * ------------------GET ROUTES
  */
-
+router.get('/', (req, res, next) => {
+    res.send("please do something interesting with yourself");
+})
 
 router.get('/login', (req, res, next) => {
-    res.send('leave a message here')
+    res.send('leave a message here');
 })
 
 router.get('/register', (req, res, next) => {
-    const form = 'there is a form here, have patience.'
+    const form = '<h1>Register Page</h1><form method="post" action="register">\
+                    Enter Username:<br><input type="text" name="username">\
+                    <br>Enter Password:<br><input type="password" name="password">\
+                    <br><br><input type="submit" value="Submit"></form>';
 
     res.send(form);
 })
@@ -63,4 +65,6 @@ router.get('/protected-route', (req, res, next) => {
 router.get('/logout', (req,res, next) => {
     req.logout();
     res.redirect('/protected-route');
-})
+});
+
+module.exports = router;
